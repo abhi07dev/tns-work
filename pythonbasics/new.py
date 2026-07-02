88ef925c141c2544ba19000fc011969472430041
abhi07dev <abhijitmadne07@gmail.com>
2026-07-02 15:45:56 +0530

    2nd push

---
+++ pythonbasics/new.py
@@ -0,0 +1,22 @@
+# Creating Variables
+i = 5
+print(i)
+str1 = "My name is John Doe"
+print(str1)
+flt = 12.678
+print(flt)
+bln = True
+print(bln)
+
+# Print the Type of each of the variables
+print(type(i))
+print(type(str))
+print(type(flt))
+print(type(bln))
+
+# A string value is passed without any problem 
+i = 5
+print(i)
+i = "Changed to a String"
+print(i)
+
