7865ed55baf83d4c998ad7e04435713194e560aa
abhi07dev <abhijitmadne07@gmail.com>
2026-07-14 23:19:15 +0530

    TNS.23.1st.py

---
+++ TNS.25.1st.py
@@ -0,0 +1,85 @@
+""" print("Hello Woeld");
+
+name= "Sushi_paglu";
+address= "dholakpur";
+age = "99";
+
+print("my name is:",name);
+print("I stay in :",address);
+print("my age is :", age);
+
+stdobj = "i am \tFrontend \n developer";
+#print(stdobj.upper());
+#print(stdobj.lower());
+
+Srint(stdobj.split());
+
+name = "jadia ";
+
+print(name.replace("Sushi_paglu","jadia"));
+
+print(name.length());
+      
+
+      
+name = input("Enter your Name:");
+
+print(name.upper())
+
+
+
+
+name =  input("Enter your password");
+if(len(name)< 9):
+    print("Your password is short");
+else:
+    print("Your are gay");
+    
+"""
+"""
+
+name= ["sushi", "Nun", "john cena","king gidora", "donald trump"]
+name.append("king godzilla")
+
+#print(name)
+#name.pop()
+#name.count()
+
+#name.count("donald trump")
+print(name.count["donald trump"])
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
